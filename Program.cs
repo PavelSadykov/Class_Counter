661bed5a3449b1fb0a7a25dfc3b8bbda31037fc4
Pavel Sadykov <2439747@gmail.com>
2023-06-14 21:06:44 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+namespace Задание__8;
+
+class Counter
+{
+    private static int count = 0;
+    private int instanceNumber;
+
+    public Counter()
+    {
+        count++;
+        instanceNumber = count;
+    }
+
+    public int GetInstanceNumber()
+    {
+        return instanceNumber;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Counter counter1 = new Counter();
+        Console.WriteLine("Номер экземпляра counter1: " + counter1.GetInstanceNumber());
+
+        Counter counter2 = new Counter();
+        Console.WriteLine("Номер экземпляра counter2: " + counter2.GetInstanceNumber());
+    }
+}
